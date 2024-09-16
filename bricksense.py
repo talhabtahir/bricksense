8f024653e9ae371ffabd32dd54e7ae647cac0ac9
Talha Bin Tahir <talhabtahir@gmail.com>
2024-09-16 14:46:57 +0500

    Update bricksense.py

--- bricksense.py
+++ bricksense.py
@@ -244,8 +244,8 @@ else:
                         img2=contours_with_border,
                         label1="Uploaded Image",
                         label2="Cracks Localization",
-                        show_labels=True,
-                        width =1300
+                        show_labels=True
+                        
                     )
                 else:
                    image_comparison(
@@ -253,8 +253,8 @@ else:
                         img2=image_with_border,
                         label1="Uploaded Image",
                         label2="Cracks Localization",
-                        show_labels=True,
-                        width =1300
+                        show_labels=True
+                    
                         )
                 st.image(image_with_border, caption="Image with Border", use_column_width=True)
                 st.image(contours_with_border, caption="Image with Border", use_column_width=False)
