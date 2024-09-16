21ff800588922745fd35e06fb7347a552f8f0fc4
Talha Bin Tahir <talhabtahir@gmail.com>
2024-09-16 20:39:33 +0500

    Update bricksense.py

--- bricksense.py
+++ bricksense.py
@@ -280,7 +280,7 @@ else:
                                 label1="Uploaded Image",
                                 label2="Cracks Localization",
                                 show_labels=False,
-                                width=660
+                                width=665
                             )
                 # with st.expander("Original vs Cracked Slider"):
                 #     # Define a maximum width for the images based on a rough estimate or browser inspection
