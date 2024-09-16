9a3aafa983dedfbdc5712ba5206c12aad722588c
Talha Bin Tahir <talhabtahir@gmail.com>
2024-09-16 17:47:56 +0500

    Update bricksense.py

--- bricksense.py
+++ bricksense.py
@@ -113,7 +113,7 @@ def add_canvas(image, fill_color=(255, 255, 255)):
     # Calculate the position to paste the image at the center of the canvas
     paste_position = (
         (canvas_width - image_width) // 2,
-        (canvas_height - image_height) // 5
+        (canvas_height - image_height) // 6
     )
     
     # Paste the original image onto the canvas
