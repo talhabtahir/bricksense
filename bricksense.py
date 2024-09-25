ab18bfa83fb458416736b373fa7c4395c3143a98
Talha Bin Tahir <talhabtahir@gmail.com>
2024-09-25 11:09:41 +0500

    Update bricksense.py

--- bricksense.py
+++ bricksense.py
@@ -74,13 +74,22 @@ st.write("")  # Creates a blank line
 # st.write(" ")  # Creates an extra line for more space
 # st.write(" ")  # Adjust the number of empty lines for desired spacing
 
+# Custom CSS to adjust the width of the selectbox
+st.markdown("""
+    <style>
+    .stSelectbox > div:first-child {
+        width: 150px !important;  /* Adjust this value to change the width */
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
 # Add a dropdown for model selection
-model_option = st.selectbox(
+model_option = st.selectbox(    
     "Choose a model for prediction:",
     ("230kmodelv11_version_cam_2", "170kmodelv10_version_cam_1")
 )
 # Show selected model below the logo
-st.write(f"### Selected Model: {model_option}")
+# st.write(f"Selected Model: {model_option}")
 
 # Sidebar navigation with icons
 st.sidebar.image("static/sidelogo.png", width=200, use_column_width=True)
