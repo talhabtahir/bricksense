15d6831063b28d7c962e5b3760be0b7f2436166b
Talha Bin Tahir <talhabtahir@gmail.com>
2024-09-25 11:05:26 +0500

    Update bricksense.py

--- bricksense.py
+++ bricksense.py
@@ -74,11 +74,11 @@ st.write("")  # Creates a blank line
 # st.write(" ")  # Creates an extra line for more space
 # st.write(" ")  # Adjust the number of empty lines for desired spacing
 
-# # Add a dropdown for model selection
-# model_option = st.selectbox(
-#     "Choose a model for prediction:",
-#     ("230kmodelv11_version_cam_2", "170kmodelv10_version_cam_1")
-# )
+# Add a dropdown for model selection
+model_option = st.selectbox(
+    "Choose a model for prediction:",
+    ("230kmodelv11_version_cam_2", "170kmodelv10_version_cam_1")
+)
 # Show selected model below the logo
 st.write(f"### Selected Model: {model_option}")
 
@@ -88,12 +88,12 @@ st.sidebar.markdown("### ")
 st.sidebar.markdown("### ")
 st.sidebar.markdown("### ")
 
-#Model selection Toolbar
-st.sidebar.markdown("### Model Selection")
-model_option = st.sidebar.selectbox(
-    "Select the model to use:",
-    ("230kmodelv11_version_cam_2", "170kmodelv10_version_cam_1")
-)
+# #Model selection Toolbar
+# st.sidebar.markdown("### Model Selection")
+# model_option = st.sidebar.selectbox(
+#     "Select the model to use:",
+#     ("230kmodelv11_version_cam_2", "170kmodelv10_version_cam_1")
+# )
 
 @st.cache_resource
 # For model comparison with sidebar selection
